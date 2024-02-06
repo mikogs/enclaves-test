97775fc355414b619119e506149b789d1253a812
Mikolaj Gasior <mikolaj@gasior.dev>
2024-02-06 12:39:50 +0100

    Extend disk

--- qemu-vm/download.sh
+++ qemu-vm/download.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 wget https://cloud.debian.org/images/cloud/bookworm/20240201-1644/debian-12-generic-amd64-20240201-1644.qcow2
+qemu-img resize debian-12-generic-amd64-20240201-1644.qcow2 +10G
 
